6c5ba102b96e9ee189112cc8d7c1d2b9b7253ff0
OlAnty <sunderson.ao@gmail.com>
2025-05-23 12:27:27 +0400

    Minor fix

--- xray-proxy-admin.sh
+++ xray-proxy-admin.sh
@@ -393,6 +393,7 @@ manage_watchdog() {
       else
         printf "${RED}Watchdog not running.${NC}\n"
       fi
+      ;;
 #      pkill -f "$WATCHDOG_SCRIPT" && printf "${GREEN}Watchdog stopped.${NC}\n" || printf "${RED}Watchdog not running.${NC}\n";;
     3) grep -Ev '^#|^$' "$WATCHDOG_SCRIPT" 2>/dev/null || printf "${RED}No config found.${NC}\n" ;;
     0) return ;;
